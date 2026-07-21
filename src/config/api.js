d6db26fd3645c58fa6a791f8e0ce8a1436c5cdad
trisula-digiart <kanzalistianm@gmail.com>
2026-07-21 23:41:14 +0700

    Create api.js

---
+++ src/config/api.js
@@ -0,0 +1,4 @@
+// src/config/api.js
+// Tempat menyimpan URL Web App Google Apps Script (GAS) dan kunci API
+
+export const GAS_API_URL = "https://script.google.com/macros/s/AKfycbyJJp3CVGiAEkCQ-6zDTgS1Rz2Fz2vQYCvpn_hB-JkN13q9aWQOAFfAtpWH3cHnby6LEg/exec"; // Contoh: https://script.google.com/macros/s/AKfycbx.../exec
